aea4e5b8c8c3632bacb9a85590bc98c9ff50311c
David Wagner <mnem@noiseandheat.com>
2022-10-31 16:52:08 +0000

    Tidy and prettify

--- Selectotron/ViewController.swift
+++ Selectotron/ViewController.swift
@@ -9,45 +9,91 @@ import UIKit
 import UniformTypeIdentifiers
 
 class ViewController: UIViewController {
-    @IBOutlet var label: UILabel!
+    @IBOutlet var textView: UITextView!
+    @IBOutlet var button: UIButton!
+
+    @IBOutlet var allowPNG: UISwitch!
+    @IBOutlet var allowJPG: UISwitch!
+    @IBOutlet var allowPDF: UISwitch!
+    @IBOutlet var preferModal: UISwitch!
+    
+    var log = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = ""
+        
+        textView.layer.cornerRadius = 8
+        textView.contentInset = .init(top: 8, left: 8, bottom: 8, right: 8)
+        
+        button.layer.cornerRadius = 8
+        button.contentEdgeInsets = .init(top: 8, left: 8, bottom: 8, right: 8)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        render(log: log)
+    }
+}
+
+extension ViewController {
+    private func clearLog() {
+        log.removeAll()
+        render(log: log)
+    }
+    
+    private func log(_ message: String) {
+        log.append(message)
+        render(log: log)
+    }
+    
+    private func render(log: [String]) {
+        DispatchQueue.main.async { [output = self.textView] in
+            output?.text = log.joined(separator:"\n\n")
+        }
     }
 }
 
 extension ViewController {
     @IBAction func handleSelectTapped(sender: UIButton) {
-        let controller = UIDocumentPickerViewController(
-            forOpeningContentTypes: [
-                .png,
-                .jpeg,
-                .pdf,
-            ]
-        )
+        clearLog()
+        
+        var mediaTypes: [UTType] = []
+        if allowPNG.isOn {
+            mediaTypes.append(.png)
+        }
+        if allowJPG.isOn {
+            mediaTypes.append(.jpeg)
+        }
+        if allowPDF.isOn {
+            mediaTypes.append(.pdf)
+        }
+
+        let controller = UIDocumentPickerViewController(forOpeningContentTypes: mediaTypes)
+
         controller.delegate = self
         controller.allowsMultipleSelection = false
         controller.shouldShowFileExtensions = true
-        present(controller, animated: true) {
-            print("Presented")
+        controller.modalPresentationStyle = preferModal.isOn ? .fullScreen : .automatic
+
+        present(controller, animated: true) { [weak self] in
+            self?.log("Presented")
         }
     }
 }
 
 extension ViewController: UIDocumentPickerDelegate {
-    
     func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
         guard let url = urls.first,
               url.startAccessingSecurityScopedResource()
         else {
-            print("Could not access resource")
+            log("Could not access resource")
             return
         }
         
+        log("Acquired security scope")
         defer {
             url.stopAccessingSecurityScopedResource()
-            print("Released security scope")
+            log("Released security scope")
         }
         
         do {
@@ -59,18 +105,17 @@ extension ViewController: UIDocumentPickerDelegate {
             
             try FileManager.default.copyItem(at: url, to: tmp)
             
-            print("Picked: \(url)")
-            print("Copied to: \(tmp)")
-            print("Type: \(tmp.mimeType)")
-            
-            label.text = "Copied \(url.pathComponents.last ?? "''") locally ready to upload."
+            log("Picked: \(url)")
+            log("Copied to: \(tmp)")
+            log("Type: \(tmp.mimeType)")
+            log("Copied \(url.pathComponents.last ?? "''") locally ready to upload.")
         } catch {
-            print("Error: \(error)")
+            log("⛔️ Error: \(error)")
         }
     }
 
     func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
-        print("Cancelled")
+        log("Cancelled")
     }
 }
 
